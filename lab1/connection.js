68bb2f7b6fc138a61ff330dcf689a46e3818dbc7
IP-14Nezdolii <vladislav07007@gmail.com>
2025-09-03 14:43:02 +0300

    new commit

---
+++ lab1/connection.js
@@ -0,0 +1,118 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const msgTypes = {
+    JOIN: 0,
+    MESSAGE: 1,
+    ID: 2,
+};
+
+export function doServer(wss, ws, rooms) {
+    let room = null;
+    let person = null;
+
+    ws.on("message", (data) => {
+        const msg = JSON.parse(data.toString());
+
+        switch (msg.msg_type) {
+            case msgTypes.JOIN: {
+                join(msg);
+                broadcast({
+                    msg_type: msgTypes.JOIN,
+                    users: [...room.persons].map(p => ({ name: p.name, id: p.id })),
+                });
+                break;
+            };
+
+            case msgTypes.MESSAGE: {
+                broadcast({
+                    msg_type: msgTypes.MESSAGE,
+                    user: person.name,
+                    id: person.id,
+                    text: msg.text,
+                });
+                break;
+            };
+        }
+    });
+
+    ws.on("close", () => {
+        quitRoom();
+        console.log("❌ Connection closed");
+    });
+
+    function join(msg) {
+        quitRoom();
+
+        person = newPerson(msg.user, ws);
+
+        if (rooms.has(msg.room)) {
+            room = rooms.get(msg.room);
+        } else {
+            room = new ServerRoom(msg.room);
+            rooms.set(msg.room, room);
+        }
+
+        room.joinRoom(person);
+
+        const message = JSON.stringify({
+            msg_type: msgTypes.ID,
+            id: person.id,
+        });
+
+        ws.send(message);
+    }
+
+    function broadcast(msg) {
+        const message = JSON.stringify(msg);
+        const clients = new Set([...room.persons].map(p => p.client));
+
+        wss.clients.forEach((client) => {
+            if (client.readyState === ws.OPEN && clients.has(client)) {
+                client.send(message);
+            }
+        });
+    }
+
+    function quitRoom() {
+        if (room &&
+            rooms.has(room.getName())) {
+
+            room.quitRoom(person);
+
+            if (room.isEmpty()) {
+                rooms.delete(room);
+            }
+        }
+    }
+
+    function newPerson(name, client) {
+        return {
+            id: uuidv4(),
+            name: name,
+            client: client,
+        }
+    }
+};
+
+class ServerRoom {
+    constructor(name) {
+        this.name = name;
+        this.persons = new Set();
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    quitRoom(person) {
+        this.persons.delete(person);
+    }
+
+    joinRoom(person) {
+        this.persons.add(person);
+    }
+
+    isEmpty() {
+        return this.persons.size === 0;
+    }
+}
---
+++ lab1/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import http from "http";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { WebSocketServer } from "ws";
+import { doServer } from "./connection.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const __indexPath = path.resolve(__dirname, "public", "index.html");
+const PORT = 8080;
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+const rooms = new Map();
+
+app.get("/page", (_, res) => {
+    fs.readFile(__indexPath, (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Error loading page");
+        }
+        res.setHeader("Content-Type", "text/html");
+        res.send(data);
+    });
+});
+
+wss.on("connection", (ws) => {
+    console.log("🔗 New connection");
+    doServer(wss, ws, rooms);
+});
+
+server.listen(PORT, () => {
+    console.log(`✅ Server page: http://localhost:${PORT}/page`);
+});
+
+
